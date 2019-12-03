5b81d0be1fd86843350fbdbdf48ecffb766fef5e
Muhammad Aiman <43446168+Wic180030@users.noreply.github.com>
2019-12-03 17:13:50 +0800

    Add files via upload

--- Train_Train/src/train_train/Queue_train.java
+++ Train_Train/src/train_train/Queue_train.java
@@ -3,5 +3,14 @@ package train_train;
 
 
 public class Queue_train {
+    Ticket_information Jan1 [] = new Ticket_information [10];
+    Ticket_information Jan2 [] = new Ticket_information [10];
+    Ticket_information Jan3[] = new Ticket_information [10];
+    Ticket_information Jan4 [] = new Ticket_information [10];
+    Ticket_information Jan5 [] = new Ticket_information [10];
+    Ticket_information Jan6 [] = new Ticket_information [10];
+    Ticket_information Jan7 [] = new Ticket_information [10];
     
+    Ticket_information node =;
+    Jan1[0]= new Ticket_information("AI",9907);
 }
